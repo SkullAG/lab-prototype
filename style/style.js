fe7720cce7ff243de9f7520843e8e760b620fef8
SkullAG <>
2021-04-23 09:03:46 +0000

    LO HE HECHO RESPONSIVE (anhelo la muerte)

---
+++ style/style.js
@@ -0,0 +1,20 @@
+
+function loadEvents()
+{
+	var mystring = getComputedStyle(document.getElementById("game")).width;
+	mystring = mystring.replace('px','');
+
+	document.getElementById("gameBorder").style.height = (3/4*mystring) + "px";
+
+	window.addEventListener('resize', resize)
+}
+
+function resize()
+{
+	var mystring = getComputedStyle(document.getElementById("game")).width;
+	mystring = mystring.replace('px','');
+
+	document.getElementById("gameBorder").style.height = (3/4*mystring) + "px";
+}
+//document.getElementById("myBtn").style.height = "50px";
+//resize
