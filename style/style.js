221bf8165822e2baddaa3388d9c21ac39719dec1
SkullAG <>
2021-04-24 01:14:15 +0000

    abrazar al dulce caos

--- style/style.js
+++ style/style.js
@@ -1,14 +1,12 @@
 
 function loadEvents()
 {
-	var mystring = getComputedStyle(document.getElementById("game")).width;
-	mystring = mystring.replace('px','');
-
-	document.getElementById("gameBorder").style.height = (3/4*mystring) + "px";
+	resize();
 
 	//alert(document.getElementById("game")).margin)
 
 	window.addEventListener('resize', resize)
+	suffer()
 }
 
 function resize()
@@ -17,6 +15,24 @@ function resize()
 	mystring = mystring.replace('px','');
 
 	document.getElementById("gameBorder").style.height = (3/4*mystring) + "px";
-}
+
+	mystring = mystring/16
+
+	//console.log(document.getElementById("gameBorder").style)
+	document.getElementById("gameBorder").style.borderWidth = (5.5*mystring)+"px "+mystring+"px "+mystring+"px "+mystring+"px "
+}//border-width = borderWidth
 //document.getElementById("myBtn").style.height = "50px";
 //resize
+
+var asciiart
+
+function suffer()
+{
+	fetch('style/suffer1.txt')
+	.then(response => response.text())
+	.then(data => {
+		// Do something with your data
+		document.getElementById("asciiShit").innerHTML = data
+		console.log(data);
+	});
+}
